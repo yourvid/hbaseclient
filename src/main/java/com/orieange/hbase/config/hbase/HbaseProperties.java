7a7dce47f7d1f5f888431698be51752bcf6c2383
pingchuanweiwei@126.com <332489188y>
2020-04-13 20:59:39 +0800

    修改

--- src/main/java/com/orieange/hbase/config/hbase/HbaseProperties.java
+++ src/main/java/com/orieange/hbase/config/hbase/HbaseProperties.java
@@ -7,10 +7,21 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "hbase")
 public class HbaseProperties {
     private Map<String, String> config;
+    private Map<String, String> phoenix;
+
     public Map<String, String> getConfig() {
         return config;
     }
+
     public void setConfig(Map<String, String> config) {
         this.config = config;
+    }
+
+    public Map<String, String> getPhoenix() {
+        return phoenix;
+    }
+
+    public void setPhoenix(Map<String, String> phoenix) {
+        this.phoenix = phoenix;
     }
 }
